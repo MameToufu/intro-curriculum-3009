9840b860f4ea8d62338f7a07936baf6fc02a4801
MameToufu <cityworkonly@gmail.com>
2020-02-02 15:47:19 +0900

    実装

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
+// 挨拶を30回表示する
 for (let count = 0; count < 30; count++) {
   fs.appendFileSync(fileName, 'こんにちは\n', 'utf8', () => { });
   fs.appendFileSync(fileName, 'こんばんは\n', 'utf8', () => { });
